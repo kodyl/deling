133b20e106710900bc781aeebcd6016a2437c6bb
Daniel Juhl <danieljuhl@gmail.com>
2015-03-27 13:17:55 +0100

    added initial version, including build and README.md

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # deling
+
+Simple app-wide configuration. Based on a simply and pre-defined pattern files are read and merged into the configuration object, which is exposed directly from the module.
+
+No need to configure `deling` before it works - simple `import` your `deling` (the Danish word for 'sharing' and for the military unit 'platoon').
+
+## Install
+
+`npm install --save deling`
+
+## Configuration
+
+You don't need to configure `deling`, but you have to configure your app - and that's what we are going to do.
+
+## Usage
+
+### Create configuration
+
+`deling` will look in the root of your project (by traverse module.parent) for config files, either in a directory named `config` or directly in the root.  If the directory `config` exists `deling` will NOT be looking in the root for config files.
+
+The content of each file will be merged into the config object, overwriting existing values for everything other than nested objects which will be deep merged.
+
+
+```
+# order of files in ./config
+./config/default.{js,json}
+./config/{NODE_ENV}.{js,json}
+./config/local.{js,json}
+
+# order of files ./
+./config.{js,json}
+./config.{NODE_ENV}.{js,json}
+./config.local.{js,json}
+```
+
+### Default configuration
+
+`deling` automatically adds some data about your project before merging in content from you config files.
+
+```
+{
+  root: '/absolute/path/to/your/application',
+  env: process.env.NODE_ENV || 'development',
+  name: pkgJson.name || '',
+  version: pkgJson.version || ''
+}
+```
+
+### Access configuration
+
+`var config = require('deling');` or the ES6 way `import config from 'deling';`
+
+`config` is a reference to your combined config based on your current environment.
+
+That's it... sorry if you hoped for more work - but `deling` is all about making configuration consitent and easy through out your application.
+
---
+++ src/index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+import fs from 'fs';
+import path from 'path';
+
+const exists = fs.existsSync;
+
+function isObject (obj) {
+  return ( typeof obj === 'object' && !Array.isArray(obj) );
+}
+
+// Deep assign
+function assign (target, source) {
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).reduce((target, key) => {
+      if (!target.hasOwnProperty(key)) {
+        target[key] = source[key];
+      }
+      else {
+        target[key] = isObject(source[key]) ? assign(target[key], source[key]) : source[key];
+      }
+      return target;
+    }, target);
+  }
+
+  return target;
+}
+
+function getPackageJson(d) {
+  console.log(d);
+  var p = path.join(d, 'package.json')
+  return exists(p) ? require(p) : false
+}
+
+function getRoot () {
+  let pkgJson = false;
+
+  if (!!~__dirname.indexOf('node_modules')) {
+    let dir = module.id;
+
+    while (path.basename(dir) !== 'node_modules') {
+      dir = path.dirname(dir);
+    }
+    return [ path.dirname(dir), pkgJson ];
+  }
+
+  let dir = module;
+
+  while (dir.parent && !pkgJson) {
+    dir = dir.parent;
+    pkgJson = getPackageJson(path.dirname(dir.filename));
+  }
+  return [ path.dirname(dir.filename), pkgJson ];
+}
+
+function getBaseInfo () {
+  const [ rootPath, pkgJson ] = getRoot();
+
+  return [
+    process.env.NODE_ENV || 'development',
+    rootPath,
+    pkgJson || getPackageJson(rootPath)
+  ];
+}
+
+function deling () {
+  const [ env, root, pkgJson ] = getBaseInfo();
+
+  const config = {
+//    pkgJson,
+    root,
+    env,
+    name: pkgJson.name || '',
+    version: pkgJson.version || ''
+  };
+
+  const files = [];
+
+  // Located in config-directory
+  if (exists(path.join(root, 'config'))) {
+    files.push(
+      path.join(root, 'config', 'default'),
+      path.join(root, 'config', env),
+      path.join(root, 'config', 'local')
+    );
+  }
+  // Located in root
+  else {
+    files.push(
+      path.join(root, 'config'),
+      path.join(root, 'config.' + env),
+      path.join(root, 'config.local')
+    );
+  }
+
+  function readFile (file) {
+    try {
+      return require(file);
+    }
+    catch (err) {
+      return {};
+    }
+  }
+
+  files.map(readFile).reduce(assign, config);
+
+  return config;
+}
+
+export default deling();
